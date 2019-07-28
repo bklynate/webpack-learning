a33503c6b5f60860fb7f885fc0c38531c31c854d
bklynate <nathanielcfa@gmail.com>
2019-07-28 18:13:57 -0400

    add example of code splitting

--- src/index.js
+++ src/index.js
@@ -7,13 +7,21 @@ import buttonCss from './button.css';
 import makeImage from './image';
 import image from './webpack-logo.jpg';
 
+const getFooter = () => import('./footer');
+
 console.log(image);
 
 const webpackImage = makeImage(image);
 const button = makeButton('Nathaniel Is Learning!');
 button.style = makeColorStyle('crimson');
 document.body.appendChild(button);
-document.body.appendChild(footer);
 document.body.appendChild(webpackImage);
+
+button.addEventListener('click', () => {
+  getFooter().then(footerModule => {
+    console.log(footerModule);
+    document.body.appendChild(footerModule.footer);
+  });
+});
 
 console.log(nav(), top, bottom, makeColorStyle('cyan'));
